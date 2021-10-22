3265b2e48895756e927cefe715a9594df4ccc2f2
Hoooooou-Jun <jipkim2@gmail.com>
2021-10-23 00:24:18 +0900

    initail setting_2

--- build/src/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const routes = (app) => {
-    app.get('/main', (req, res) => {
-        res.sendStatus(200);
-    });
-};
-exports.default = routes;
