d0a9437e42e342bd1ce870358c9d53da660d1b18
Hoooooou-Jun <jipkim2@gmail.com>
2021-10-25 22:06:35 +0900

    중복되는 username 예외처리

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -15,31 +15,40 @@ const validateToken = (req: Request, res: Response, next: NextFunction) => {
 
 const register = (req: Request, res: Response, next: NextFunction) => {
     let { username, password } = req.body
-    bcryptjs.hash(password, 10, (hashError, hash) => {
-        if (hashError) {
-            return res.status(500).json({
-                message: hashError.message,
-                error: hashError
+    User.find({ username }).exec().then((users) => {
+        if (users.length) { 
+            return res.status(405).json({
+                message: 'Exist username'
+            })
+        }
+        else {
+            bcryptjs.hash(password, 10, (hashError, hash) => {
+                if (hashError) {
+                    return res.status(500).json({
+                        message: hashError.message,
+                        error: hashError
+                    });
+                }
+                const _user = new User({
+                    _id: new mongoose.Types.ObjectId(),
+                    username,
+                    password: hash
+                });
+        
+                return _user.save().then((user) => {
+                    return res.status(200).json({
+                        user
+                    });
+                })
+                .catch((error) => {
+                    return res.status(500).json({
+                        message: error.message,
+                        error
+                    });
+                });
             });
         }
-        const _user = new User({
-            _id: new mongoose.Types.ObjectId(),
-            username,
-            password: hash
-        });
-
-        return _user.save().then((user) => {
-            return res.status(201).json({
-                user
-            });
-        })
-        .catch((error) => {
-            return res.status(500).json({
-                message: error.message,
-                error
-            });
-        });
-    });
+    })
 };
 
 const login = (req: Request, res: Response, next: NextFunction) => {
@@ -47,28 +56,29 @@ const login = (req: Request, res: Response, next: NextFunction) => {
     User.find({ username }).exec().then((users) => {
         if (users.length !== 1) {
             return res.status(401).json({
-                message:'Unauthorized'
+                message:'Unauthorized user'
             });
         }
         bcryptjs.compare(password, users[0].password, (error, result) => {
             if (error) {
                 logger.error(error.message)
                 return res.status(401).json({
-                    message:'Unauthorized'
+                    message:'Incorrect password'
                 });  
             }
             else if (result) {
                 signJWT(users[0], (_error, token) => {
-                    if(_error) {
+                    if (_error) {
                         logger.error("Unable to sign token", _error)
                         return res.status(401).json({
-                            message:'Unauthorized',
+                            message:'Unauthorized token',
                             error: _error
                         });
                     }
                     else if (token) {
+                        logger.info(`Login Success '${username}'`)
                         return res.status(200).json({
-                            message: 'Auth Success',
+                            message: 'Authorize Success',
                             token,
                             user: users[0]
                         })
--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -2,14 +2,13 @@ import mongoose from 'mongoose';
 import config from 'config'
 import logger from './logger'
 
-const NAMESPACE = "Server";
-
 const connect = async () => {
     try {
         await mongoose.connect(config.get<string>('mongo.DBURL'), config.get<{}>('mongo.option'));
         logger.info("DB connected")
     } catch(error) {
-        logger.error(NAMESPACE, "Could not connect to db", error);
+        logger.error("Could not connect to db");
+        logger.error(error)
         process.exit(1);
     }
 };
